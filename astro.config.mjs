2920611f3d7df128741f104d61dfdb1d5708c610
Matthew Savin <matt.asavin@gmail.com>
2024-10-27 15:00:26 +0000

    attempt SSR in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://hacksheffield.uk',
-    //output: 'hybrid'
+    output: 'server',
+    adapter: node({
+        mode: "standalone"
+    })
 });
